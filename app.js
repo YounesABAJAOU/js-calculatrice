ad2a37ba410ad1b67a26b76677ce70f414650d42
YounesABAJAOU <younes-abajaou14@hotmail.com>
2022-02-10 09:28:20 +0100

    feat: fonction maths

---
+++ app.js
@@ -0,0 +1,19 @@
+function addition(nb1, nb2){
+    let calcul = nb1+nb2;
+    alert(calcul);
+}
+
+function soustraction(nb1, nb2){
+    let calcul = nb1-nb2;
+    alert(calcul);
+}
+
+function division(nb1, nb2){
+    let calcul = nb1/nb2;
+    alert(calcul);
+}
+
+function mulltiplication(nb1, nb2){
+    let calcul = nb1*nb2;
+    alert(calcul);
+}
